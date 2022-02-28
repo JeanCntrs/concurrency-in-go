f2cf90bb2cdd771799927aa64fc0daec285ca032
Jean C. Contreras <jean.cntrs@gmail.com>
2022-02-28 18:14:41 -0300

    Was added solution of race condition using atomic var

--- 04-race_condition/atomic_solution/main.go
+++ 04-race_condition/atomic_solution/main.go
@@ -3,30 +3,27 @@ package main
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 )
 
 var (
-	wg              sync.WaitGroup
-	mutex                 = sync.Mutex{}
+	wg sync.WaitGroup
+
 	widgetInventory int32 = 1000 //Package-level variable will avoid all the pointers
 )
 
 func main() {
 	fmt.Println("Starting inventory count = ", widgetInventory)
-
 	wg.Add(2)
 	go makeSales()
 	go newPurchases()
 	wg.Wait()
-
 	fmt.Println("Ending inventory count = ", widgetInventory)
 }
 
 func makeSales() { // 1000000 widgets sold
 	for i := 0; i < 300000; i++ {
-		mutex.Lock()
-		widgetInventory -= 100
-		mutex.Unlock()
+		atomic.AddInt32(&widgetInventory, -100)
 	}
 
 	wg.Done()
@@ -34,10 +31,7 @@ func makeSales() { // 1000000 widgets sold
 
 func newPurchases() { // 1000000 widgets purchased
 	for i := 0; i < 300000; i++ {
-		mutex.Lock()
-		widgetInventory += 100
-		mutex.Unlock()
+		atomic.AddInt32(&widgetInventory, 100)
 	}
-
 	wg.Done()
 }
